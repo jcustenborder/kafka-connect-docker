6633be36883ef0bb2c673fa3697170c0a745df06
Jeremy Custenborder <jcustenborder@gmail.com>
2021-01-29 14:28:59 -0600

    Switched to use multiple repositories.

--- Jenkinsfile
+++ Jenkinsfile
@@ -15,13 +15,6 @@ node {
     def dockerImage = "docker.custenborder.com/jcustenborder/jenkins-packer-ansible-python:0.0.1"
 
     stage('generate') {
-        sh "mkdir build"
-        dir('build') {
-            git branch: 'master', changelog: false, credentialsId: '50a4ec3a-9caf-43d1-bfab-6465b47292da', poll: false, url: 'git@github.com:jcustenborder/kafka-connect-all.git'
-            sh 'git config user.email "jenkins@custenborder.com"'
-            sh 'git config user.name "Jenkins"'
-        }
-
         withDockerRegistry(credentialsId: 'custenborder_docker', url: 'https://docker.custenborder.com') {
             docker.image(dockerImage).inside() {
                 sh "pip install -r requirements.txt"
@@ -31,11 +24,47 @@ node {
     }
 
     stage('push') {
-        dir('build') {
-            sh "echo `git add --all . && git commit -m 'Build ${BUILD_NUMBER}' .`"
-            sshagent(credentials: ['50a4ec3a-9caf-43d1-bfab-6465b47292da']) {
-                sh "git push 'git@github.com:jcustenborder/kafka-connect-all.git' master"
-            }
+        def workSpaceRoot = new File(pwd())
+        def buildRoot = new File(pwd(), "build")
+        def repositoriesFile = new File(buildRoot, "repositories.json")
+        def repoRoot = new File(pwd(), "repos")
+
+        sh "mkdir ${repoRoot}"
+
+        def repositories = new JsonSlurperClassic().parse(repositoriesFile)
+
+        repositories.each {
+            def name = it['name']
+            def imageDirectory = new File(repoRoot, name)
+            sh "mkdir ${imageDirectory}"
+            def path = it['path']
+            def branchBuild = new File(workSpaceRoot, path)
+            def branch = it['branch']
+            def branchDirectory = new File(imageDirectory, branch)
+            sh "mkdir ${branchDirectory}"
+            def repositoryUrl = it['repository_url']
+            sh "copy -rv ${branchBuild}/ ${branchDirectory}/"
         }
+
+
+
+
+//        sh "mkdir build"
+//        dir('build') {
+//            git branch: 'master', changelog: false, credentialsId: '50a4ec3a-9caf-43d1-bfab-6465b47292da', poll: false, url: 'git@github.com:jcustenborder/kafka-connect-all.git'
+//            sh 'git config user.email "jenkins@custenborder.com"'
+//            sh 'git config user.name "Jenkins"'
+//        }
+
+
     }
+
+//    stage('push') {
+//        dir('build') {
+//            sh "echo `git add --all . && git commit -m 'Build ${BUILD_NUMBER}' .`"
+//            sshagent(credentials: ['50a4ec3a-9caf-43d1-bfab-6465b47292da']) {
+//                sh "git push 'git@github.com:jcustenborder/kafka-connect-all.git' master"
+//            }
+//        }
+//    }
 }
--- generator.py
+++ generator.py
@@ -1,10 +1,10 @@
-import yaml
+import json
 import os.path
 import re
 import urllib.request
-import json
-
 from os import path
+
+import yaml
 
 settings = None
 
@@ -48,6 +48,9 @@ def api_plugin(api_owner, api_name):
     return result
 
 
+repositories = []
+repositories_file_path = path.join(output_root, 'repositories.json')
+
 for image, imageSettings in settings['images'].items():
     print(f"Processing {image}")
     image_root = path.join(output_root, image)
@@ -59,10 +62,31 @@ for image, imageSettings in settings['images'].items():
     for image_version, base_image_version in imageSettings['versions'].items():
         version_root = path.join(image_root, image_version)
 
+        repository = {
+            'name': image,
+            'path': version_root,
+            'branch': image_version,
+            'repository_url': imageSettings['repository']
+        }
+        repositories.append(repository)
         if not path.exists(version_root):
             os.mkdir(version_root)
 
         docker_file_path = path.join(version_root, "Dockerfile")
+        readme_file_path = path.join(version_root, "README.md")
+
+        download_plugins = {}
+        if 'plugins' in imageSettings:
+            for plugin in imageSettings['plugins']:
+                owner = plugin['owner']
+                plugin = plugin['plugin']
+                hub_contents = api_plugins(owner)
+
+                for hub_plugin in hub_contents:
+                    plugin_name = hub_plugin['name']
+                    if re.match(plugin, plugin_name):
+                        plugin_info = api_plugin(owner, plugin_name)
+                        download_plugins[f"{owner}/{plugin_name}"] = plugin_info
 
         with open(docker_file_path, "+w") as dockerFile:
             print(f"FROM {from_image}:{base_image_version}", file=dockerFile)
@@ -75,28 +99,27 @@ for image, imageSettings in settings['images'].items():
                 for key, value in imageSettings['env'].items():
                     print(f"ENV {key}={value}", file=dockerFile)
 
-            if 'plugins' in imageSettings:
-                download_plugins = []
-                for plugin in imageSettings['plugins']:
-                    owner = plugin['owner']
-                    plugin = plugin['plugin']
-                    hub_contents = api_plugins(owner)
-                    # if owner in hub_cache:
-                    #     hub_contents = hub_cache[owner]
-                    # else:
-                    #     hub_url = f"https://api.hub.confluent.io/api/plugins/{owner}"
-                    #     print(f"Downloading {hub_url}")
-                    #     with urllib.request.urlopen(hub_url) as url:
-                    #         hub_contents = json.loads(url.read().decode())
-                    #         hub_cache[owner] = hub_contents
+            for download_plugin, plugin_info in sorted(download_plugins.items()):
+                print(
+                    f"RUN confluent-hub install --no-prompt {download_plugin}:{plugin_info['version']}",
+                    file=dockerFile)
 
-                    for hub_plugin in hub_contents:
-                        plugin_name = hub_plugin['name']
-                        if re.match(plugin, plugin_name):
-                            plugin_info = api_plugin(owner, plugin_name)
-                            plugin_version = plugin_info['version']
-                            download_plugin = f"{owner}/{plugin_name}:{plugin_version}"
-                            download_plugins.append(download_plugin)
+        with open(readme_file_path, "+w") as readmeFile:
+            if 'readme' in imageSettings:
+                for header, content in imageSettings['readme'].items():
+                    print(f"# {header}\n", file=readmeFile)
+                    print(f"{content}", file=readmeFile)
 
-                for download_plugin in sorted(download_plugins):
-                    print(f"RUN confluent-hub install --no-prompt {download_plugin}", file=dockerFile)
+            print("", file=readmeFile)
+            print("| Plugin | Version | Documentation | Source |", file=readmeFile)
+            print("|--------|---------|---------------|--------|", file=readmeFile)
+
+            for download_plugin, plugin_info in sorted(download_plugins.items()):
+                hub_url = f"https://www.confluent.io/hub/{download_plugin}"
+
+                print(
+                    f"| [{download_plugin}]({hub_url}) | {plugin_info['version']} | [Documentation]({plugin_info['documentation_url']}) | [Source]({plugin_info['source_url']}) |",
+                    file=readmeFile)
+
+with open(repositories_file_path, "+w") as repositories_file:
+    repositories_file.write(json.dumps(repositories, indent=4))
